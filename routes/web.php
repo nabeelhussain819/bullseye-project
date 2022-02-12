8c134bf8c401b8f1466026added4a0b161eb1269
Syed Armash Hussain <armashash@gmail.com>
2022-02-12 21:02:34 +0500

    Removed laravel screen when web page first loads

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth'])->prefix('app')->group(function () {
     Route::apiResources([
         'surveys' => \App\Http\Controllers\SurveyController::class,
     ]);
 });
-
-
-Route::get('/{any}', function () {
-    return view('index');
-})->where('any', '.*');
